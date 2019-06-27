2e09417b8141b831b18b8d6eb10b5e4c0885595d
Alex Carberry <alex.carberry@epicgames.com>
2019-06-27 15:52:08 -0400

    update alfred pub key location

--- scripts/alfred.sh
+++ scripts/alfred.sh
@@ -4,7 +4,7 @@
 adduser alfred
 mkdir -p /home/alfred/.ssh
 touch /home/alfred/.ssh/authorized_keys
-curl -L 'https://raw.githubusercontent.com/ajcarberry/homelab/master/baseImages/ssh/alfred.pub' -o /home/alfred/.ssh/authorized_keys
+curl -L 'https://raw.githubusercontent.com/ajcarberry/packer-ubuntu-1804/feature-initial/keys/alfred.pub' -o /home/alfred/.ssh/authorized_keys
 chmod 700 /home/alfred/.ssh
 chmod 600 /home/alfred/.ssh/authorized_keys
 chown -R alfred:alfred /home/alfred/.ssh
